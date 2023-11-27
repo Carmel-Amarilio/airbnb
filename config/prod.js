05407399c6763584dfe3318be382a300dae9d9b0
Carmel Amarilio <carmelamarilio@gmail.com>
2023-11-27 13:48:12 +0200

    B/connect the app to atlas

--- config/prod.js
+++ config/prod.js
@@ -1,4 +1,4 @@
 export default {
-    dbURL: 'mongodb+srv://carmel:<carmel123>@cluster0.u6ghyzl.mongodb.net/?retryWrites=true&w=majority',
+    dbURL: 'mongodb+srv://carmel:carmel123@cluster0.u6ghyzl.mongodb.net/?retryWrites=true&w=majority',
     dbName: 'airbnb',
 }
